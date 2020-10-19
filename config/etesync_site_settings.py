6e8183370b46c36af65bf26b750e6e5ced8376fb
BeatLink <beatlink+git@simplelogin.co>
2020-10-18 20:43:45 -0500

    Add code to create secret.txt if not existent

--- config/etesync_site_settings.py
+++ config/etesync_site_settings.py
@@ -1,10 +1,16 @@
 import os
+import secrets
 
 data_directory = os.environ.get("ETESYNC_DATA_DIRECTORY")
 database_file = os.environ.get("ETESYNC_DATABASE_FILE")
 
 ALLOWED_HOSTS = ["*"]
 SECRET_FILE = os.path.join(data_directory, "secret.txt")
+try:
+    open(SECRET_FILE)
+except FileNotFoundError:
+    with open(SECRET_FILE, 'w') as file:
+        file.write(secrets.token_urlsafe(256))
 
 DATABASES = {
     "default": {
